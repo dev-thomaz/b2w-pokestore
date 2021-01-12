04b45455a743e0a4afe02c0e6d83b2aed3c1dbe2
thomaz <>
2021-01-12 15:20:17 -0300

    search bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ const Header: React.FC<HeaderProps> = (props) => {
   const [showSearch, setShowSearch] = useState(false)
 
   useEffect(() => {
-    window.location.pathname.split('/')[1] === 'cart' || window.location.pathname.split('/')[1] === 'favorites' || window.location.pathname.split('/')[1] === 'orders' || window.location.pathname.split('/')[1].length < 1 || isPokemonPage() ? setShowSearch(false) : setShowSearch(true)
+    window.location.pathname.split('/')[1] === 'cart' || window.location.pathname.split('/')[1] === 'favorites' || window.location.pathname.split('/')[1] === 'orders' || window.location.pathname.split('/')[1].length < 1  ? setShowSearch(false) : setShowSearch(true)
     
     if(location !== window.location.pathname){
       setLocation(window.location.pathname)
@@ -22,51 +22,7 @@ const Header: React.FC<HeaderProps> = (props) => {
   },[location])
 
 
-  const isPokemonPage = () => {
 
-    switch( window.location.pathname.split('/')[1]){
-      case 'normal':
-      return true;
-      case 'fighting':
-        return true;
-      case 'flying':
-        return true;
-      case 'poison':
-        return true;
-      case 'ground':
-        return true;
-      case 'rock':
-        return true;
-      case 'bug':
-        return true;
-      case 'ghost':
-        return true;
-      case 'steel':
-        return true;
-      case 'fire':
-        return true;
-      case 'water':
-        return true;
-      case 'grass':
-        return true;
-      case 'electric':
-        return true;
-      case 'psychic':
-        return true;
-      case 'ice':
-        return true;
-      case 'dragon':
-        return true;
-      case 'dark':
-        return true;
-      case 'fairy':
-        return true;
-      
-        default:
-          return false
-    }
-
-  }
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     props.searchPokemon(event.target.value)
@@ -90,6 +46,7 @@ const Header: React.FC<HeaderProps> = (props) => {
       <div>
         <Link to='/'> <HiHome /></Link>
         <Link to='/cart'><HiShoppingCart /></Link>
+        <Link to='/favorites'><HiHeart /></Link>
         <Link to='/orders'><RiFilePaper2Line /></Link>
       </div>
     </Container>
